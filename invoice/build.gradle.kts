860f5bf4dfae1a9664b446fd0958d0f6ff888956
Rohi Noferesti <roohinofrsti@gmail.com>
2025-07-29 16:15:50 +0400

    add company and itext dependencies

--- invoice/build.gradle.kts
+++ invoice/build.gradle.kts
@@ -7,9 +7,11 @@ dependencies {
     implementation(project(":tenant"))
     implementation(project(":contact"))
     implementation(project(":ledger"))
+    implementation(project(":company"))
 
     implementation("org.springframework:spring-context")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+    implementation("com.itextpdf:itext7-core:9.2.0")
     runtimeOnly("org.postgresql:postgresql")
 }
