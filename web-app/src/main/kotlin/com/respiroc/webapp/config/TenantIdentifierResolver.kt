eb3aa3b2dbb7d7c9c12185c8fcf96c52f8537973
hassan210302 <hassan210302@gmail.com>
2025-08-02 21:41:25 +0500

    resolving comments - schedular removed

--- web-app/src/main/kotlin/com/respiroc/webapp/config/TenantIdentifierResolver.kt
+++ web-app/src/main/kotlin/com/respiroc/webapp/config/TenantIdentifierResolver.kt
@@ -1,6 +1,5 @@
 package com.respiroc.webapp.config
 
-import com.respiroc.product.application.ProductSyncService
 import com.respiroc.util.context.SpringUser
 import org.hibernate.cfg.AvailableSettings
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver
@@ -16,8 +15,6 @@ class TenantIdentifierResolver : CurrentTenantIdentifierResolver<Long>, Hibernat
     }
 
     override fun resolveCurrentTenantIdentifier(): Long {
-        //To Bypass multiTenancy we need to do this
-        ProductSyncService.getCurrentTenantId()?.let { return it }
         val auth = SecurityContextHolder.getContext().authentication ?: return UNKNOWN_TENANT_ID
         val principal = auth.principal
         if (principal !is SpringUser) return UNKNOWN_TENANT_ID
